b83f9c697f67571749ee06936036d379715e10cd
raygerlabs <raygerlabs@gmail.com>
2022-08-03 19:27:01 +0200

    Add build job

--- src/glu.lua
+++ src/glu.lua
@@ -1,51 +1,51 @@
 -- file: glu.lua
 --
---local context = require("lustache.context")
---
 -- Remove the trailing spaces from the specified string
 --
 string.trim = function(s)
   return s:match('^%s*(.*%S)') or ''
 end
 --
--- Define glu module
---
 local glu = {filters={}}
 --
--- Process the current parameter in the filter expression:
+-- Process the parameters in the filter expression:
 -- {{ value | filter : param1 : param2 : param3 }}
 --
 function glu:parse_param(param, context)
   local stringExp = "^[\'\"](.*)[\'\"]$"
   local numericExp = "^[+-]?%d+$"
   local decimalExp = "^[+-]?%d%.%d+$"
-  if param:match(stringExp) then -- Remove single and double quotes around the string
+  if param:match(stringExp)
+  then
     return param:gsub(stringExp, "%1")
   end
-  if param:match(numericExp) then
-    return math.tointeger(param) -- Available since Lua 5.3; we must preserve backward compatibility somehow...
-  end
-  if param:match(decimalExp) then -- All numbers are decimals in Lua
+  if param:match(decimalExp)
+  then
     return tonumber(param)
   end
-  return context:lookup(param) -- If none of the above is fulfilled then search the cache
+  if param:match(numericExp)
+  then
+    return tonumber(param)
+  end
+  return context:lookup(param)
 end
 --
--- Resolves a single filter# in a mustache expression:
+-- Resolve filter# in a mustache expression:
 -- {{ value | filter1 | filter2 | .. | filterN }}
 --
 function glu:exec_filter(expression, filter, context)
   local params = {expression}
   local tokens = string.split(filter, ":")
-  local filter_name = string.trim(table.remove(tokens, 1)) -- The first element in the table is always the filter!
-  for _, token in ipairs(tokens) do -- Assemble the parameter list for the filter function
+  local filter_name = string.trim(table.remove(tokens, 1)) -- The first element is always the filter!
+  for _, token in ipairs(tokens) do -- Assemble the parameter list
     token = string.trim(token)
     token = self:parse_param(token, context)
     table.insert(params, token)
   end
-  if self.filters[filter_name] ~= nil then -- Call the filter function
+  if self.filters[filter_name] ~= nil -- Check whether the specified filter exists or not
+  then
     filter = self.filters[filter_name]
-    expression = filter(table.unpack(params))
+    expression = filter(table.unpack(params)) -- Call the filter
   end
   return expression
 end
@@ -56,16 +56,16 @@ local context = require "lustache.context"
 local _lookup = context.lookup
 function context.lookup(self, name)
   local tokens = string.split(name, "|")
-  local expression = table.remove(tokens, 1)  -- The first part of the expression is always the mustache expression
+  local expression = table.remove(tokens, 1)  -- The first element is always the mustache expression!
   expression = string.trim(expression)
-  expression = _lookup(self, expression) -- Process the expression as usual
-  for _, token in ipairs(tokens) do -- Apply the filters
+  expression = _lookup(self, expression) -- Search the cache in the context
+  for _, token in ipairs(tokens) do -- Apply filters
     expression = glu:exec_filter(expression, token, self)
   end
   return expression
 end
 --
--- Construct the glue plugin.
+-- Construct the extension plugin
 --
 function glu:new(filters)
   local instance = {}
